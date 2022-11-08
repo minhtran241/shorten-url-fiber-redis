22a2bfd167c1c5fdf451e8755b90233c2b4b51b0
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2022-11-08 00:30:11 -0500

    implement rate limiting

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,11 +1,15 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 
+	"github.com/minhtran241/shorten-url-fiber-redis/database"
 	"github.com/minhtran241/shorten-url-fiber-redis/helpers"
 )
 
@@ -26,21 +30,44 @@ type response struct {
 func ShortenURL(c *fiber.Ctx) error {
 	body := new(request)
 	if err := c.BodyParser(&body); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid JSON format"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid JSON format",
+		})
 	}
 
 	// implement rate limiting
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	value, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*time.Minute).Err()
+	} else {
+		valInt, _ := strconv.Atoi(value)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":           "rate limit exceeded",
+				"rate_limit_rest": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
 
 	// check if the input is an actual URL
 	if !govalidator.IsURL(body.URL) {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid URL"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid URL",
+		})
 	}
 
 	// check for domain error
 	if !helpers.RemoveDomainError(body.URL) {
-		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": ""})
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error": "infinity loop detected",
+		})
 	}
 
 	// enforce https, SSL
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	r2.Decr(database.Ctx, c.IP())
 }
